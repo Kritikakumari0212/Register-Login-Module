d57700ab1c9faf73986b7ae198a3ac5682ff5030
Kritika <kritikakumari7774@gmail.com>
2025-11-13 15:21:28 +0530

    login

--- index.js
+++ index.js
@@ -13,15 +13,24 @@ const server = http.createServer((req, res) => {
       // let data=fs.readFileSync("./src/Register.html")
       // res.end(data)
       renderHtml("./src/Register.html", res);
-    } else if(req.url=="/login") {
-        renderHtml("./src/Login.html", res);
-    }
-    else if (req.url.endsWith(".css")) {
+    } else if (req.url == "/login") {
+      renderHtml("./src/Login.html", res);
+    } else if (req.url.endsWith(".css")) {
       let fileName = req.url.slice(1);
       renderHtml(`./src/${fileName}`, res);
     }
   } else if (req.method == "POST") {
     //all post requests
+    if (req.url == "/submit-register") {
+      let data = "";
+      req.on("data", (chunk) => {
+        data += chunk;
+      });
+      req.on("end", () => {
+        console.log(data);
+        res.end("Data Recieved, See console for data");
+      });
+    }
   } else {
     res.end("no route found");
   }
